f2b028ff327af36c3e201081949d774db660b2fb
Rickard Ahlstedt <rickard.re.ahlstedt@gmail.com>
2012-09-04 12:36:29 +0200

    Added a few things.

--- index.php
+++ index.php
@@ -2,13 +2,33 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
+        <title>WebText</title>
         <link rel="stylesheet"
         type="text/css" href="style/style.css" />
     </head>
+    <script>
+    $(document).ready(function(){
+        
+    });
+    </script>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="wrapper">
+            <div id="nav">
+                Navbar
+            </div>
+            <div id="content">
+                I am content!
+            </div>
+            <div id="user">
+                <div class="ver">
+                    User
+                </div>
+                <div id="user_inner">
+                    User
+                </div>
+            </div>
+        </div>
     </body>
 </html>
